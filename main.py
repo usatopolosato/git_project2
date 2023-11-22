1df76c77305a0dd8506c654a6233e6c5246029e9
Ivan_Ryabtsev <ryabts777@yandex.ru>
2023-11-22 15:28:56 +0300

    Third commit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+def my_function():
+    print('Нужно решать задачи')
+
+
 def main():
     print('My first git program')
     print('And I change it every day')
-    
+
 
 if __name__ == '__main__':
     main()
